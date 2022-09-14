4428f7090afbbbc8efc583bdfd6aaafe08869b09
Selva kumar <selvaec107@gmail.com>
2022-09-14 23:11:22 +0200

    Add logs groovy

---
+++ vars/logs.groovy
@@ -0,0 +1,39 @@
+#!/usr/bin/env groovy
+
+def loadcolors() {
+RED='\033[0;31m' 
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+ON_YELLOW='\033[43m' 
+NC='\033[@m'
+}
+
+def info(message) {
+loadColors() 
+sh """set +X; echo -e "${CYAN} [INFO] - $message ${NC}" """
+}
+
+def info2(message) {
+loadColors()
+sh """set +x; echo -e "${PURPLE} [INFO] - $message ${NC}" """ 
+}
+
+def warn(message) {
+loadColors()
+sh "*"set +x; echo -e "${ON_YELLOW} [WARN] - $message ${NC}" "" 
+}
+
+
+def error(message, throwException) {
+loadColors) 
+sh"" "set +x; echo -e "${RED) [ERROR] - $message ${NC}" """
+if (throwException) {
+    throw new RuntimeException(message)
+}
+}
+def debug(message) {
+loadColors) sh"" "set +x; echo -e "${BLUE} [DEBUG] - $message ${NC}" """
+}
+
